6faed5fb317f65dafc7685a3fdc264b09e1cfacc
Michael Greene <michaelduffgreenejr@gmail.com>
2020-09-29 12:40:39 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+
+console.log('some text');
+let avg = findAvg(2, 2);
+console.log('some text', avg);
+function findAvg(a, b) {
+    console.log('some text');
+    var answer = ( a + b) / 2;
+    return answer;
+}
+
+
+let fruits = ["apples", "plums", "grapes"];
+let leastFav =fruits[2]
+// let favFruit;
+fruitPrint();
+// favFruitPrint();
+console.log(leastFav)
+function fruitPrint() {
+  let favFruit = fruits[1];
+  
+    
+  function favFruitPrint() {
+    console.log("my favorite fruits are " + favFruit);
+    
+}
+favFruitPrint ()
+console.log(fruits[0]);
+}
+funcTown ()
+function funcTown (){
+    let name='Michael';
+    console.log('hello '+name);
+};
+
+let greeting =function () {
+    alert("Greetings...");
+}
+greeting()
